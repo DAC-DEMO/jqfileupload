f645ea6de726398440dffe4b16d18f7398c5d00b
santosh <santosh.ece06@gmail.com>
2017-07-23 23:24:26 +0530

    upload update.

--- main.js
+++ main.js
@@ -1,17 +1,30 @@
 $(function() {
-    /*$('#fileupload').fileupload({
-        url: "/server/upload",
-        dataType: 'json',
-        send: function(e, data) {
-            console.log("send");
-        }
-    });*/
+    var serverUrl = "http://localhost:8080/baba/springs/rest/upload";
 
     $('#fileupload').fileupload({
-        dataType: 'json',
-        add: function(e, data) {
+        "url": serverUrl,
+        "dataType": "json",
+
+        "submit": function(e, data) {
+            var formData = [{
+                "name": "username",
+                "value": "sana"
+            }, {
+                "name": "password",
+                "value": "fondu"
+            }];
+            data.formData = formData;
+        },
+        "add": function(e, data) {
             $("#uploadBtn").off('click').on('click', function() {
-                data.submit();
+                // server call starts here
+                var jqXHR = data.submit();
+                jqXHR.success(function(result, textStatus, jqXHR) {
+                    console.log("SUCCESS");
+                });
+                jqXHR.error(function(jqXHR, textStatus, errorThrown) {
+                    console.log("FAILURE");
+                });
             });
         }
     });
